28cccdd6692f91f771bbd2d65bf8822a679b8cb1
Miguel Martinez <miguelitalia@hotmail.com>
2018-05-17 14:21:51 +0000

    question and answer challenge

---
+++ some.py
@@ -0,0 +1,19 @@
+questions = {
+    "who's going to win the world cup": "japan",
+    "who's going to be runner up in the world cup": "germany"
+}
+
+
+counter = 0
+
+for k, v in questions.items():
+    
+    user_answer = input(k)
+    
+    if user_answer == v:
+        print("Correct answer")
+        counter += 1
+    else:
+        print("incorrect answer")
+        
+print(counter)   
